d64736faa3b7ed4cda48193effa46c31f04d525d
Ikram Shaikh <ikramshaikh1648@gmail.com>
2025-12-28 16:04:40 +0530

    Rename Navbar import to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/NavBar";
 import { Intro } from "./components/Intro";
 import { About } from "./components/About";
 import { Skills } from "./components/Skills";
